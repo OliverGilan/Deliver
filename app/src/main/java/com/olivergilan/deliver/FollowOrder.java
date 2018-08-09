e4c8e1ed1e777f56eab418462427f1111f9720b7
Oliver Gilan <olivergilan@gmail.com>
2018-08-09 16:39:49 -0400

    Added chat functionality to complete order and remove references from database

--- app/src/main/java/com/olivergilan/deliver/FollowOrder.java
+++ app/src/main/java/com/olivergilan/deliver/FollowOrder.java
@@ -1,10 +1,12 @@
 package com.olivergilan.deliver;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -15,7 +17,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.StringTokenizer;
+
+import javax.annotation.Nullable;
 
 public class FollowOrder extends AppCompatActivity {
 
@@ -29,6 +37,7 @@ public class FollowOrder extends AppCompatActivity {
     private ChatAdapter adapter;
     private ImageButton sendBtn;
     private EditText messageCompose;
+    private Button receivedOrderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,7 @@ public class FollowOrder extends AppCompatActivity {
         chatList = (ListView) findViewById(R.id.messageList);
         sendBtn = (ImageButton) findViewById(R.id.messageSendBtn);
         messageCompose = (EditText) findViewById(R.id.messageTextEdit);
+        receivedOrderBtn = (Button) findViewById(R.id.orderReceived);
 
         chatRef = getIntent().getExtras().getString("chatPath");
 
@@ -53,6 +63,41 @@ public class FollowOrder extends AppCompatActivity {
                 sendMessage();
             }
         });
+
+        receivedOrderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(FollowOrder.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                String orderRef = "";
+                String chars = "/";
+                StringTokenizer st = new StringTokenizer(chatRef, chars, true);
+                while(st.hasMoreTokens()){
+                    String cheese = st.nextToken();
+                    if(cheese.matches("chats")){
+                        orderRef += "activeOrders";
+                    }else{
+                        orderRef += cheese;
+                    }
+                }
+                Log.i("CHATS", orderRef);
+                database.document(orderRef).delete();
+                database.document(chatRef).delete();
+                startActivity(intent);
+            }
+        });
+
+        database.document(chatRef).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if(e != null){
+                    Log.i("CHAT", "Failed to listen");
+                }
+                if(documentSnapshot != null && documentSnapshot.exists()){
+                    loadMessages();
+                }
+            }
+        });
     }
 
     private void loadMessages(){
@@ -62,6 +107,9 @@ public class FollowOrder extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
+                        if(document.get("Arrived") != null){
+                            receivedOrderBtn.setVisibility(View.VISIBLE);
+                        }
                         chatData = document.toObject(ChatData.class);
                         Log.i("CHAT", chatData.getHead().toString());
                         adapter = new ChatAdapter(FollowOrder.this, chatData);
--- app/src/main/java/com/olivergilan/deliver/OrderChat.java
+++ app/src/main/java/com/olivergilan/deliver/OrderChat.java
@@ -1,10 +1,12 @@
 package com.olivergilan.deliver;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -20,9 +22,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import javax.annotation.Nullable;
 
 public class OrderChat extends AppCompatActivity {
 
@@ -36,6 +42,7 @@ public class OrderChat extends AppCompatActivity {
     private ChatAdapter adapter;
     private ImageButton sendBtn;
     private EditText messageCompose;
+    private Button orderDoneBtn;
 
 
     @Override
@@ -51,19 +58,41 @@ public class OrderChat extends AppCompatActivity {
         chatList = (ListView) findViewById(R.id.messageList);
         sendBtn = (ImageButton) findViewById(R.id.messageSendBtn);
         messageCompose = (EditText) findViewById(R.id.messageTextEdit);
+        orderDoneBtn = (Button) findViewById(R.id.orderDone);
 
         loadMessages();
 
         if(getIntent().hasExtra("arrived")){
             messageCompose.setText("I have arrived with your package!");
-            sendMessage();
             alert();
+            orderDoneBtn.setVisibility(View.VISIBLE);
+            orderDoneBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(OrderChat.this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                }
+            });
         }
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendMessage();
+            }
+        });
+
+        database.document(chatRef).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w("ORDERS", "Listen failed.", e);
+                    return;
+                }
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    loadMessages();
+                } else {
+                    Log.d("CHAT", "Current data: null");
+                }
             }
         });
     }
